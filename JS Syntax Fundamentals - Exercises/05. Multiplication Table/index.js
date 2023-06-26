a946a543af3558fbd80e3946a31c9ceef2b9a27b
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-06-26 18:48:31 +0300

    update

---
+++ JS Syntax Fundamentals - Exercises/05. Multiplication Table/index.js
@@ -0,0 +1,7 @@
+function muliplacation(number){
+    for(let index=1;index<=10;index++){
+        const result=number*index;
+        console.log(`${number} X ${index} = ${result}`)
+    }
+}
+muliplacation(5)
