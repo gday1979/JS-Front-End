8bb681577c6025a8183ee9885ed5418e85f51559
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-07-13 19:23:56 +0300

    update

--- JS DOM and Events Lab/05. Delete-From-Table/app.js
+++ JS DOM and Events Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,22 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let input = document.querySelector('input[name="email"]');
+    let email = input.value;
+    let table = document.querySelector('tbody');
+    let rows = Array.from(table.children);
+    let result = document.getElementById('result');
+
+    let isDeleted = false;
+    for (let row of rows) {
+        let rowEmail = row.children[1].textContent;
+        if (rowEmail === email) {
+            row.remove();
+            isDeleted
+}
+    }
+    if (isDeleted) {    
+        result.textContent = 'Deleted.';
+    }
+    else {
+        result.textContent = 'Not found.';
+    }
 }
