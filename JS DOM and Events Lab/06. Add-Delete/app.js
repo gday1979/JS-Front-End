9c81b1e33820a20bcdc482ab7470655646d71c84
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-07-14 18:43:19 +0300

    update

--- JS DOM and Events Lab/06. Add-Delete/app.js
+++ JS DOM and Events Lab/06. Add-Delete/app.js
@@ -1,3 +1,16 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById('newText');
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    let a = document.createElement('a');
+    a.textContent = '[Delete]';
+    a.href = '#';
+    a.addEventListener('click', deleteItem);
+    li.appendChild(a);
+    document.getElementById('items').appendChild(li);
+    input.value = '';
+
+    function deleteItem() {
+        li.remove();
+    }
 }
