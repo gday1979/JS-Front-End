ce73078cd60a12b375262b525cf661f122844caa
gday1979 <97783032+gday1979@users.noreply.github.com>
2023-07-21 15:04:09 +0300

    update

--- JS DOM-and-Events Exercises/07. Format the Text/solution.js
+++ JS DOM-and-Events Exercises/07. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input').value;
+  let output = document.getElementById('output');
+
+  let sentences = input.split('.').filter(x => x !== '');
+  let paragraphs = [];
+
+  while (sentences.length > 0) {
+    let paragraph = sentences.splice(0, 3).join('. ') + '.';
+    paragraphs.push(paragraph);
+  }
+
+  for (let i = 0; i < paragraphs.length; i++) {
+    let p = document.createElement('p');
+    p.textContent = paragraphs[i];
+    output.appendChild(p);
+  }
 }
